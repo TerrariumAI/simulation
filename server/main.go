41d7be0b80e747499d01cfb70864610d8e84eef1
Zac Holland <zacharyholland@gmail.com>
2019-02-04 15:58:37 -0700

    moved server out of main

--- server/main.go
+++ server/main.go
@@ -36,21 +36,6 @@ type SpawnAgentWithNewAgentIdChan struct {
 	chNewAgentId chan int32
 }
 
-// Server represents the gRPC server
-type Server struct {
-	agents        map[int32]*Entity
-	chAgentSpawn  chan SpawnAgentWithNewAgentIdChan
-	chAgentAction chan pb.AgentActionRequest
-
-	// Observer id to use for next observer
-	observerId int32
-	// Entity id to use for next entity
-	entityId int32
-
-	// Map from observer id to their observation channel
-	observerationChannels map[int32]chan EntityUpdate
-}
-
 func main() {
 	// Initialize server obj
 	var simulationServer = Server{
@@ -74,17 +59,4 @@ func main() {
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
-}
-
-// TEMPORARY UNTIL OBSV SERVICE IS DEVELOPED
-// Returns the next observer's id and increases by 1
-func (s *Server) GetObserverId() (id int32) {
-	s.observerId += 1
-	return s.observerId
-}
-
-// Returns the next entity's id and increases by 1
-func (s *Server) GetEntityId() (id int32) {
-	s.entityId += 1
-	return s.entityId
 }
