fb6bbf5d6b9fbf473131ef480b993b55cd3c18af
Zac Holland <zacharyholland@gmail.com>
2019-03-06 17:03:58 -0700

    updated proto compile scripts

--- third_party/protoc-gen-js.sh
+++ third_party/protoc-gen-js.sh
@@ -1,20 +1,23 @@
+#!/bin/bash
+JS_OUT=../web-client/pkg/api/v1
+
 protoc -I/usr/local/include -I. \
  --proto_path=api/proto/v1 \
  --proto_path=third_party \
- --js_out=import_style=commonjs:pkg/api/v1/js \
- --grpc-web_out=import_style=commonjs,mode=grpcwebtext:pkg/api/v1/js \
+ --js_out=import_style=commonjs:$JS_OUT \
+ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$JS_OUT \
 simulation-service.proto
 
 protoc -I/usr/local/include -I. \
  --proto_path=api/proto/v1 \
  --proto_path=third_party \
- --js_out=import_style=commonjs:pkg/api/v1/js \
- --grpc-web_out=import_style=commonjs,mode=grpcwebtext:pkg/api/v1/js \
+ --js_out=import_style=commonjs:$JS_OUT \
+ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$JS_OUT \
 google/api/annotations.proto google/api/http.proto
 
 protoc -I/usr/local/include -I. \
  --proto_path=api/proto/v1 \
  --proto_path=third_party \
- --js_out=import_style=commonjs:pkg/api/v1/js \
- --grpc-web_out=import_style=commonjs,mode=grpcwebtext:pkg/api/v1/js \
+ --js_out=import_style=commonjs:$JS_OUT \
+ --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$JS_OUT \
 protoc-gen-swagger/options/annotations.proto protoc-gen-swagger/options/openapiv2.proto
