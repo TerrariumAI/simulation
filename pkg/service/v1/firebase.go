b0493ad4b6689db5f982729cbc13f3f74e7d1899
Zac Holland <zacharyholland@gmail.com>
2019-04-02 12:38:00 -0600

    updated Readme

--- pkg/service/v1/firebase.go
+++ pkg/service/v1/firebase.go
@@ -41,53 +41,6 @@ func initializeFirebaseApp(env string) *firebase.App {
 	return app
 }
 
-// func authenticateFirebaseAccountWithIDToken(ctx context.Context, app *firebase.App, env string) *auth.Token {
-// 	// get the auth token from the context
-// 	md, ok := metadata.FromIncomingContext(ctx)
-// 	if !ok {
-// 		return nil
-// 	}
-// 	authTokenHeader, ok := md["auth-token"]
-// 	if !ok {
-// 		logger.Log.Warn("verifyFirebaseIDToken(): No auth-token header in context")
-// 		return nil
-// 	}
-// 	idToken := authTokenHeader[0]
-// 	// -----------------------------------
-// 	// TESTING FUNCTIONALITY
-// 	// -----------------------------------
-// 	if env != "prod" {
-// 		// If this is the correct testing token, return a testing token with fake uid
-// 		if idToken == "TEST-ID-TOKEN" {
-// 			return &auth.Token{
-// 				UID: "TEST-UID",
-// 			}
-// 		}
-// 		// If not correct test token, return nil
-// 		return nil
-// 	}
-// 	// -----------------------------------
-// 	// Make sure the firebase app instance exists
-// 	if app == nil {
-// 		logger.Log.Warn("Couldn't authenticate user: error initializing firebase app")
-// 		return nil
-// 	}
-// 	// Attempt to create a firebase auth client
-// 	client, err := app.Auth(context.Background())
-// 	if err != nil {
-// 		logger.Log.Warn("Error getting Auth client: %v\n", zap.String("reason", err.Error()))
-// 		return nil
-// 	}
-// 	// Verify the token
-// 	token, err := client.VerifyIDToken(ctx, idToken)
-// 	if err != nil {
-// 		logger.Log.Warn("Error verifying ID token: %v\n", zap.String("reason", err.Error()))
-// 		return nil
-// 	}
-
-// 	return token
-// }
-
 func authenticateFirebaseAccountWithSecret(ctx context.Context, app *firebase.App, env string) (map[string]interface{}, error) {
 	// get the auth token from the context
 	md, ok := metadata.FromIncomingContext(ctx)
