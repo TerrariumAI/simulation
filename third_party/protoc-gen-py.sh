c9b83eb27c2822ef7b0a645033014cc778dcbd35
Zac Holland <zacharyholland@gmail.com>
2019-05-23 16:50:05 -0600

    cleaned up the repo

--- third_party/protoc-gen-py.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PY_OUT=../python-client
-
-python3 -m grpc_tools.protoc -I/usr/local/include -I. \
-  --proto_path=api/proto/v1 \
-  --proto_path=third_party \
-  --python_out=$PY_OUT \
-  --grpc_python_out=$PY_OUT \
-google/api/annotations.proto google/api/http.proto protoc-gen-swagger/options/annotations.proto protoc-gen-swagger/options/openapiv2.proto simulation-service.proto
