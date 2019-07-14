e4faec216e612dbcf9ea282e2c1856776ced2655
Zac Holland <zacharyholland@gmail.com>
2019-07-14 09:49:51 -0700

    test pass

--- pkg/environment/environment_test.go
+++ pkg/environment/environment_test.go
@@ -86,7 +86,7 @@ func TestCreateEntity(t *testing.T) {
 
 func TestGetEntity(t *testing.T) {
 	// ctxWithoutValidToken := context.Background()
-	md := metadata.Pairs("auth-secret", "MOCK-SECRET")
+	md := metadata.Pairs("x-endpoint-api-userinfo", "{\"id\":\"mock-user-id\"}")
 	ctxWithValidSecret := metadata.NewIncomingContext(context.Background(), md)
 
 	s := NewEnvironmentServer("testing", redisAddr)
@@ -113,12 +113,11 @@ func TestGetEntity(t *testing.T) {
 			},
 			want: &api.GetEntityResponse{
 				Entity: &api.Entity{
-					Id:       "0",
-					OwnerUID: "MOCK_USER_ID",
-					ModelID:  "MOCK-MODEL-ID",
-					Class:    1,
-					X:        1,
-					Y:        1,
+					Id:      "0",
+					ModelID: "MOCK-MODEL-ID",
+					Class:   1,
+					X:       1,
+					Y:       1,
 				},
 			},
 		},
@@ -138,11 +137,79 @@ func TestGetEntity(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := tt.s.GetEntity(tt.args.ctx, tt.args.req)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("simulationService.CreateEntity() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("simulationService.GetEntity() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if err == nil && !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("environment.CreateEntity() = %v, want %v", got, tt.want)
+				t.Errorf("environment.GetEntity() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetEntitiesInRegion(t *testing.T) {
+	// ctxWithoutValidToken := context.Background()
+	md := metadata.Pairs("x-endpoint-api-userinfo", "{\"id\":\"mock-user-id\"}")
+	ctxWithValidSecret := metadata.NewIncomingContext(context.Background(), md)
+
+	s := NewEnvironmentServer("testing", redisAddr)
+
+	type args struct {
+		ctx context.Context
+		req *api.GetEntitiesInRegionRequest
+	}
+	tests := []struct {
+		name    string
+		s       api.EnvironmentServer
+		args    args
+		want    *api.GetEntitiesInRegionResponse
+		wantErr bool
+	}{
+		{
+			name: "Succesful",
+			s:    s,
+			args: args{
+				ctx: ctxWithValidSecret,
+				req: &api.GetEntitiesInRegionRequest{
+					X: 0,
+					Y: 0,
+				},
+			},
+			want: &api.GetEntitiesInRegionResponse{
+				Entities: []*api.Entity{
+					&api.Entity{
+						Id:      "0",
+						ModelID: "MOCK-MODEL-ID",
+						Class:   1,
+						X:       1,
+						Y:       1,
+					}},
+			},
+		},
+		{
+			name: "Entity doesn't exist",
+			s:    s,
+			args: args{
+				ctx: ctxWithValidSecret,
+				req: &api.GetEntitiesInRegionRequest{
+					X: 1,
+					Y: 1,
+				},
+			},
+			want: &api.GetEntitiesInRegionResponse{
+				Entities: []*api.Entity{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.s.GetEntitiesInRegion(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("simulationService.GetEntitiesInRegion() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err == nil && !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("environment.GetEntitiesInRegion() = %v, want %v", got, tt.want)
 			}
 		})
 	}
